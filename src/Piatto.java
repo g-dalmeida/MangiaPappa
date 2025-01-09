537cb616366bd14b5b10387b81d7f120233364e0
g-dalmeida <giordix23@gmail.com>
2025-01-09 09:43:29 +0100

    piatto e produttore

--- src/Piatto.java
+++ src/Piatto.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class Piatto {
+    private final ArrayList<Integer> bocconi = new ArrayList<Integer>();
+    private final int maxBocconi = 10;
+
+    public synchronized void aggiungiBoccone(Integer b) throws InterruptedException {
+        while (bocconi.size() == maxBocconi) {
+            System.out.println("Piatto pieno! Il papà aspetta...");
+            wait();
+        }
+        bocconi.add(b);
+        System.out.println("Papà aggiunge il boccone: " + b);
+        notifyAll();
+    }
+
+    public synchronized Integer mangiaBoccone() throws InterruptedException {
+        while (bocconi.isEmpty()) {
+            System.out.println("Piatto vuoto! Il bimbo aspetta...");
+            wait();
+        }
+        Integer b = bocconi.remove(0);
+        System.out.println("Il bimbo mangia il boccone: " + b);
+        notifyAll();
+        return b;
+    }
 }
+
--- src/Produttore.java
+++ src/Produttore.java
@@ -1,2 +1,22 @@
-public class Produttore {
+public class Produttore extends Thread {
+    private final Piatto piatto;
+
+    public Produttore (Piatto piatto) {
+        this.piatto = piatto;
+    }
+
+    @Override
+    public void run(){
+        Integer count = 1;
+        try {
+            while (true){
+                piatto.aggiungiBoccone(count);
+                count++;
+                Thread.sleep(500);
+            }
+        }catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+            System.out.println("Produttore interrotto");
+        }
+    }
 }
