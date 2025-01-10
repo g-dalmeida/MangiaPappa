58424c2cf0eff06f65e3104c2962c0c3f64a49d7
g-dalmeida <giordix23@gmail.com>
2025-01-10 09:37:13 +0100

    FINISH

--- src/Consumatore.java
+++ src/Consumatore.java
@@ -1,2 +1,20 @@
-public class Consumatore {
+public class Consumatore extends Thread {
+    private final Piatto piatto;
+
+    public Consumatore(Piatto piatto){
+        this.piatto = piatto;
+    }
+
+    @Override
+    public void run(){
+        try{
+            while (true){
+                    piatto.mangiaBoccone();
+                    Thread.sleep(1000);
+            }
+        }catch (Exception e){
+            Thread.currentThread().interrupt();
+            System.out.println("Consumatore interrotto");
+        }
+    }
 }
--- src/Produttore.java
+++ src/Produttore.java
@@ -1,18 +1,22 @@
 public class Produttore extends Thread {
     private final Piatto piatto;
+    private final Integer max;
 
     public Produttore (Piatto piatto) {
         this.piatto = piatto;
+        this.max = 60;
     }
 
     @Override
     public void run(){
         Integer count = 1;
+        Integer bocmax = 0;
         try {
-            while (true){
+            while (bocmax < max){
                 piatto.aggiungiBoccone(count);
                 count++;
                 Thread.sleep(500);
+                bocmax++;
             }
         }catch (InterruptedException e){
             Thread.currentThread().interrupt();
